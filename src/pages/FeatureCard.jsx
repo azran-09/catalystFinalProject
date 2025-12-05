6f345e0c505f0c22dfc07989319d2579ffb1c1c0
ising2025 <ishani.singh2025@gmail.com>
2025-12-05 13:06:53 -0500

    border animation

--- src/pages/FeatureCard.jsx
+++ src/pages/FeatureCard.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 function FeatureCard({title, description}) {
   return (
-    <motion.div className="feature-card" whileHover={{scale: 1.05}}>
+    <motion.div className="feature-card" whileHover={{scale: 1.05, borderColor: " #08806b", borderWidth: "4px",}}>
       <h3>{title}</h3>
       <p>{description}</p>
     </motion.div>
